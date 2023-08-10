573f3b30f61496138002a4c841f8cb3086ee4df7
blackleila <blackleila84@gmx.de>
2023-08-10 10:34:59 +0200

    Header und Home-Komponente mit Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./pages/home/home.component";
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [{
   path: 'home',
@@ -10,7 +10,6 @@ const routes: Routes = [{
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-
   }
 ];
 
